1b9bd00c70950a49e122f23f47615cf2e988ca43
Nele Riedesel <nele.riedesel@gmail.com>
2024-11-17 11:47:36 +0100

    Refactoring of StatusTest to StatusServiceTest

--- src/test/java/status/StatusServiceTest.java
+++ src/test/java/status/StatusServiceTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class StatusTest {
+public class StatusServiceTest {
 
     private StatusService statusService;
 
